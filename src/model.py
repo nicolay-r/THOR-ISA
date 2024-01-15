2e9c6a2164ba9fae8e57f426c8018bd54d046187
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-01-15 13:22:08 +0000

    Refactoring to take a correct label

--- src/model.py
+++ src/model.py
@@ -33,5 +33,5 @@ class LLMBackbone(nn.Module):
         output = self.engine.generate(input_ids=input_ids, attention_mask=input_masks, max_length=200)
         dec = [self.tokenizer.decode(ids) for ids in output]
         label_dict = {w: self.config.label_map[i] for i, w in enumerate(self.config.label_list)}
-        output = [label_dict.get(w.replace('<pad>', '').replace('</s>', '').strip(), 0) for w in dec]
+        output = [label_dict.get(w.replace('<pad>', '').replace('</s>', '').strip(), label_dict["neutral"]) for w in dec]
         return output
