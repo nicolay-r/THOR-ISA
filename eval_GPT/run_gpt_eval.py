625e15bfc223ba121d261b054e906872619c933b
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-01-17 13:33:54 +0000

    Same for GPT

--- eval_GPT/run_gpt_eval.py
+++ eval_GPT/run_gpt_eval.py
@@ -87,11 +87,8 @@ def prepare_data(args, config):
 
 def report_score(golds, preds, mode='test'):
     res = {}
-    # res['Acc_SA'] = accuracy_score(self.golds['total'], self.preds['total'])
-    # res['F1_SA'] = f1_score(self.golds['total'], self.preds['total'], labels=[0, 1, 2], average='macro')
-    # res['F1_ESA'] = f1_score(self.golds['explicits'], self.preds['explicits'], labels=[0, 1, 2], average='macro')
-    # res['F1_ISA'] = f1_score(self.golds['implicits'], self.preds['implicits'], labels=[0, 1, 2], average='macro')
-    res["F1_PN"] = f1_score(golds['total'], preds['total'], average='macro', labels=[1, 2])
+    res['Acc_SA'] = accuracy_score(golds['total'], preds['total'])
+    res["F1_PN"] = accuracy_score(golds['total'], preds['total'], average='macro', labels=[1, 2])
     res["F1_PN0"] = f1_score(golds['total'], preds['total'], average='macro')
     res['default'] = res['F1_SA']
     res['mode'] = mode
