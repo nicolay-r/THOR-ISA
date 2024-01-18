eb5dcebd8aa60b7fb1a67bc36c2b47d0dcf0c89e
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-01-18 12:19:27 +0000

    Refactoring.

--- main.py
+++ main.py
@@ -21,13 +21,11 @@ class Template:
             setattr(config, k, v)
         config.dataname = config.data_name
         set_seed(config.seed)
-
-        if self.config.infer_iter >= -1:
-            config.shuffle = False
-
         config.device = torch.device('cuda:{}'.format(config.cuda_index) if torch.cuda.is_available() else 'cpu')
         names = [config.model_size, config.dataname] + names
         config.save_name = '_'.join(list(map(str, names))) + '_{}.pth.tar'
+        if config.infer_iter >= -1:
+            config.shuffle = False
         self.config = config
 
     def forward(self):
